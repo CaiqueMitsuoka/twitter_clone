f8a1ecd171dea249a58df9fe4f9154b90a7f947b
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-06-16 20:04:45 -0300

    Add user-tweet relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616213103) do
+ActiveRecord::Schema.define(version: 20170616230057) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "post"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
